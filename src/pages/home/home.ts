b29f09364aef0719e75a4dd9d42f7f60ccdd549e
BENEZIT Luca <luca.benezit@etu.univ-amu.fr>
2018-03-13 09:45:56 +0100

    v2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LivrePage } from '../livre/livre';
+import { ModalController, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
+  }
+
+  Ajouterlivre(){
+  	console.log("Fonction Ajouter Livre")
+  	let modal = this.modalCtrl.create(LivrePage);
+	modal.present();
   }
 
 }
